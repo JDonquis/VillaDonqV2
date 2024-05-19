e4f4dc734797488f0007d28b49f814e24ce3a79c
Donquis <juandonquis07@gmail.com>
2024-05-19 16:15:00 -0400

    matricula view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function index()
     {   
-        $courses = Course::all();
+        // $courses = Course::all();
 
-        $course_sections = new CourseSectionCollection(CourseSection::with('section','course')->get());
+        // $course_sections = new CourseSectionCollection(CourseSection::with('section','course')->get());
         
-        $studentsPerCourse = $this->studentService->getStudentsPerCourse(1);
+        // $studentsPerCourse = $this->studentService->getStudentsPerCourse(1);
         
-        return view('workspace.admin.matricula',['students' => $studentsPerCourse, 'courses' => $courses, 'course_sections' => $course_sections]);
+        return inertia('Dashboard/Matricula');
+
+        // return Inertia('workspace.admin.matricula',['students' => $studentsPerCourse, 'courses' => $courses, 'course_sections' => $course_sections]);
 
     }
 }
