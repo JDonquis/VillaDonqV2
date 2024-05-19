4f1ef80d6917526aafff71ce58e55c6045fa79e3
Donquis <juandonquis07@gmail.com>
2024-05-19 16:26:44 -0400

    return courses

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,19 +3,27 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function index()
     {   
-        // $courses = Course::all();
+        $courses = Course::all();
 
         // $course_sections = new CourseSectionCollection(CourseSection::with('section','course')->get());
         
         // $studentsPerCourse = $this->studentService->getStudentsPerCourse(1);
         
-        return inertia('Dashboard/Matricula');
+        return inertia('Dashboard/Matricula',
+        [
+            'data' =>
+            [
+                'courses' => $courses,
+            ]
+            
+        ]);
 
         // return Inertia('workspace.admin.matricula',['students' => $studentsPerCourse, 'courses' => $courses, 'course_sections' => $course_sections]);
 
