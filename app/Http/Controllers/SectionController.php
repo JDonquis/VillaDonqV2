995b18428c21168533d4b268eec0a2212f29bbee
Donquis <juandonquis07@gmail.com>
2024-05-26 14:47:20 -0400

    ni

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -6,6 +6,7 @@ use Exception;
 use App\Models\Section;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Models\CourseSection;
 use App\Services\StudentService;
 use Illuminate\Support\Facades\DB;
 
@@ -17,48 +18,17 @@ class SectionController extends Controller
      */
     public function store(Request $request)
     {   
-        $currentlastSectionName_letter = Section::select('name')->orderBy('id','desc')->first()->name;
-        $nextLetter = chr(ord($currentlastSectionName_letter) + 1);
-        
-        $sectionCreated = Section::create(['name' => $nextLetter]);
+        $nextSectionId = $request->section_id + 1;
 
-        DB::table('course_sections')->insert(['course_id' => $request->course_id, 'section_id' => $sectionCreated->id]);
+        CourseSection::create(['course_id' => $request->course_id, 'section_id' => $nextSectionId])->save(); 
 
         return redirect('/dashboard/matricula');
-
     }
 
-    public function destroy($id)
-    {   
-        DB::beginTransaction();
-
-        try 
-        {
-            $sections = Section::orderBy('id', 'desc')->limit(2)->get();
-            
-            Student::where('course_id',$id)->where('section_id',$sections[0]->id)->update(['section_id',$sections[1]->id]);
-
-            DB::table('course_sections')
-            ->where('course_id',$id)
-            ->where('section_id',$sections[0]->id)
-            ->delete();
-
-            Section::delete($sections[0]->id);
-            
-            DB::commit();
-
-            return redirect('/dashboard/matricula');
-        } 
-        catch (Exception $th) 
-        {
-            DB::rollback();
-            return redirect('/dashboard/matricula')->withErrors(['data' => 'Algo ha salido mal :(']);
-            
-        }
+    // public function destroy($id)
+    // {         
+    //     CourseSection::where('course_id',$request->course_id), 'section_id' => $nextSectionId])->save(); 
 
 
-        
-
-
-    }
+    // }
 }
