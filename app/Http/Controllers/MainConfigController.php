b8afddeb4ae83bc134d87b64bd8da14c69dab852
Donquis <juandonquis07@gmail.com>
2024-06-24 12:08:14 -0400

    ni

--- app/Http/Controllers/MainConfigController.php
+++ app/Http/Controllers/MainConfigController.php
@@ -21,7 +21,7 @@ class MainConfigController extends Controller
         [
             'data' =>
             [
-                 $accounts,
+                'accounts' => $accounts,
             ]
         
         ]
