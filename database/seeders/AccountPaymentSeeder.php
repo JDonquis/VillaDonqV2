49264f32f2bebe72cd3b800dcb7fe25772a7eeec
Juan Donquis <juandonquis07@gmail.com>
2026-04-22 22:23:56 -0400

    add efectivos

--- database/seeders/AccountPaymentSeeder.php
+++ database/seeders/AccountPaymentSeeder.php
@@ -17,7 +17,9 @@ class AccountPaymentSeeder extends Seeder
     {
         $rows =
             [
-                ['payment_method_id' => PaymentMethodEnum::Efectivo->value, 'person_name' => null, 'email' => null, 'ci' => null, 'phone_number' => null, 'bank' => null, 'account_number' => null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'comision' => null],
+                ['payment_method_id' => PaymentMethodEnum::Efectivo->value, 'person_name' => null, 'email' => null, 'ci' => null, 'phone_number' => null, 'bank' => null, 'account_number' => null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'comision' => null, 'cash_currency' => 'Bolivares'],
+                ['payment_method_id' => PaymentMethodEnum::Efectivo->value, 'person_name' => null, 'email' => null, 'ci' => null, 'phone_number' => null, 'bank' => null, 'account_number' => null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'comision' => null, 'cash_currency' => 'Dolares'],
+
                 ['payment_method_id' => PaymentMethodEnum::PagoMovil->value, 'person_name' => null, 'email' => null, 'ci' => '10478463', 'phone_number' => '04146846012', 'bank' => 'Provincial', 'account_number' => null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'comision' => null],
                 ['payment_method_id' => PaymentMethodEnum::PagoMovil->value, 'person_name' => null, 'email' => null, 'ci' => '10478463', 'phone_number' => '04146846012', 'bank' => 'BNC', 'account_number' => null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'comision' => null],
                 ['payment_method_id' => PaymentMethodEnum::Transferencia->value, 'person_name' => 'COLEGIO MAESTRO JOSÉ MARTÍ', 'email' => null, 'ci' => '10478463', 'phone_number' => '04146846012', 'bank' => 'BNC', 'account_number' => '0191-0122-34-2100102346', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'comision' => null],
