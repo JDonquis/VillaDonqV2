035dc8d1eabdafecc9f498b9a71084e1435228d2
Juan Donquis <juandonquis07@gmail.com>
2026-05-04 13:55:43 -0400

    a

--- resources/js/Pages/Dashboard/Pagos.svelte
+++ resources/js/Pages/Dashboard/Pagos.svelte
@@ -11,51 +11,51 @@
     import ColorsPayMethods from "../../components/ColorsPayMethods";
     import BalanceBar from "../../components/BalanceBar.svelte";
     export let data = { students: { data: [] }, accounts: { data: [] } };
-   
-    const balances = [
-        {
-            id: 58,
-            student_id: 58,
-            status: "pending",
-            inscription: -50,
-            january: 0,
-            january_status: "paid",
-            february: 0,
-            february_status: "paid",
-            march: -30,
-            march_status: "partially_paid",
-            april: -50,
-            april_status: "debt",
-            may: -50,
-            may_status: "pending",
-            june: -50,
-            june_status: "pending",
-            july: -50,
-            july_status: "pending",
-            august: -50,
-            august_status: "pending",
-            september: 0,
-            september_status: "paid",
-            october: 0,
-            october_status: "paid",
-            november: 0,
-            november_status: "paid",
-            december: 0,
-            december_status: "paid",
-            school_lapse_id: 1,
-            created_at: "2026-04-27T19:59:49.000000Z",
-            updated_at: "2026-04-27T19:59:49.000000Z",
-            school_lapse: {
-                id: 1,
-                start: "2026-09-01",
-                end: "2027-08-31",
-                status: 1,
-                created_at: "2026-04-27 19:59:43",
-                updated_at: "2026-04-27 19:59:43",
-            },
-        },
-    ];
-   
+
+    // const balances = [
+    //     {
+    //         id: 58,
+    //         student_id: 58,
+    //         status: "pending",
+    //         inscription: -50,
+    //         january: 0,
+    //         january_status: "paid",
+    //         february: 0,
+    //         february_status: "paid",
+    //         march: -30,
+    //         march_status: "partially_paid",
+    //         april: -50,
+    //         april_status: "debt",
+    //         may: -50,
+    //         may_status: "pending",
+    //         june: -50,
+    //         june_status: "pending",
+    //         july: -50,
+    //         july_status: "pending",
+    //         august: -50,
+    //         august_status: "pending",
+    //         september: 0,
+    //         september_status: "paid",
+    //         october: 0,
+    //         october_status: "paid",
+    //         november: 0,
+    //         november_status: "paid",
+    //         december: 0,
+    //         december_status: "paid",
+    //         school_lapse_id: 1,
+    //         created_at: "2026-04-27T19:59:49.000000Z",
+    //         updated_at: "2026-04-27T19:59:49.000000Z",
+    //         school_lapse: {
+    //             id: 1,
+    //             start: "2026-09-01",
+    //             end: "2027-08-31",
+    //             status: 1,
+    //             created_at: "2026-04-27 19:59:43",
+    //             updated_at: "2026-04-27 19:59:43",
+    //         },
+    //     },
+    // ];
+
     export let searched_students = [];
     let isSearchTableOpen = false;
     let searchInputRef;
@@ -269,6 +269,7 @@
                                         {
                                             id: student.id,
                                             name: student.name,
+                                            balances: student.balances || [],
                                             last_name: student.last_name,
                                             ci: student.ci,
                                             course_name: student.course.name,
@@ -408,10 +409,12 @@
                             </td>
                         </tr>
                         <tr>
-                        <td colspan="7">
-                            <BalanceBar balances={balances} amountToPay={student.amount_in_dolars } />
-
-                        </td>
+                            <td colspan="7">
+                                <BalanceBar
+                                    balances={student.balances}
+                                    amountToPay={student.amount_in_dolars}
+                                />
+                            </td>
                         </tr>
                     {/each}
                 </tbody>
@@ -510,7 +513,6 @@
             ></iconify-icon>{/if} Bs
     </p>
 </div>
-
 
 <Table
     {selectedRow}
