bac2e3ced8ec9afdd38388e81a3cc18db0f83f98
Juan Donquis <juandonquis07@gmail.com>
2026-05-04 15:36:47 -0400

    account payment filter backend

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -16,20 +16,20 @@ class PaymentService
             ->when(isset($params['search']), function ($q) use ($params) {
                 $search = $params['search'];
                 $q->where(function ($query) use ($search) {
-                    $query->where('reference', 'like', '%' . $search . '%')
-                        ->orWhere('observations', 'like', '%' . $search . '%')
+                    $query->where('reference', 'like', '%'.$search.'%')
+                        ->orWhere('observations', 'like', '%'.$search.'%')
                         ->orWhereHas('user', function ($q) use ($search) {
-                            $q->whereRaw("CONCAT(name, ' ', last_name) LIKE ?", ['%' . $search . '%'])
-                                ->orWhere('name', 'like', '%' . $search . '%')
-                                ->orWhere('last_name', 'like', '%' . $search . '%');
+                            $q->whereRaw("CONCAT(name, ' ', last_name) LIKE ?", ['%'.$search.'%'])
+                                ->orWhere('name', 'like', '%'.$search.'%')
+                                ->orWhere('last_name', 'like', '%'.$search.'%');
                         })
                         ->orWhereHas('accountPayment.method', function ($q) use ($search) {
-                            $q->where('name', 'like', '%' . $search . '%');
+                            $q->where('name', 'like', '%'.$search.'%');
                         })
                         ->orWhereHas('students', function ($q) use ($search) {
-                            $q->whereRaw("CONCAT(name, ' ', last_name) LIKE ?", ['%' . $search . '%'])
-                                ->orWhere('name', 'like', '%' . $search . '%')
-                                ->orWhere('last_name', 'like', '%' . $search . '%');
+                            $q->whereRaw("CONCAT(name, ' ', last_name) LIKE ?", ['%'.$search.'%'])
+                                ->orWhere('name', 'like', '%'.$search.'%')
+                                ->orWhere('last_name', 'like', '%'.$search.'%');
                         });
                 });
             })
@@ -37,7 +37,10 @@ class PaymentService
                 $q->whereDate('date', $params['date']);
             })
             ->when(isset($params['account_payment_id']), function ($q) use ($params) {
-                $q->where('account_payment_id', $params['account_payment_id']);
+                $accountPaymentIds = is_array($params['account_payment_id'])
+                    ? $params['account_payment_id']
+                    : [$params['account_payment_id']];
+                $q->whereIn('account_payment_id', $accountPaymentIds);
             });
 
         $totalIncome = (clone $query)->sum('total_in_dolars');
@@ -46,7 +49,7 @@ class PaymentService
 
         return [
             'payments' => $payments,
-            'total_income' => $totalIncome
+            'total_income' => $totalIncome,
         ];
     }
 
