5838b95d8f95eb7e35613003dab36744cdce833d
Juan Donquis <juandonquis07@gmail.com>
2026-05-21 17:14:26 -0400

    schoolLapses in dashboard

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SchoolLapse;
 use Inertia\Response;
 use Illuminate\Support\Facades\Request;
 
-class AppController 
-{   
-    
+class AppController
+{
+
 
     public function index(): Response
     {
@@ -16,13 +17,30 @@ class AppController
 
     public function dashboard(): Response
     {
-        return inertia('Dashboard/Index');
+        $schoolLapse = SchoolLapse::get();
+        return inertia('Dashboard/Index', [
+            'schoolLapses' => $schoolLapse,
+        ]);
+    }
+
+    public function annualVsMonthlyFlow(Request $request)
+    {
+        $schoolLapse = null;
+
+        if (!$request->has('schoolId')) {
+            $schoolLapse = SchoolLapse::where('status', 1)->first();
+        } else {
+            $schoolLapse = SchoolLapse::where('id', $request->input('schoolId'))->first();
+        }
+
+        return response()->json([
+            'annualFlow' => [1000, 1500, 1200, 1800, 2000, 2200, 2500, 2700, 3000, 3200, 3500, 4000],
+            'monthlyFlow' => [200, 250, 220, 300, 350, 400, 450, 500, 550, 600, 650, 700],
+        ]);
     }
 
     public function maquinas(): Response
     {
         return inertia('Dashboard/Maquinas');
     }
-
-   
 }
